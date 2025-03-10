002474160b7454bc5a69d373db129835e9a873f4
Kdiviya <eraiswaryabe@gmail.com>
2025-03-10 15:20:02 -0500

    boolean-expressions-and-conditional-statements.js

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,25 +28,57 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+const hasTent = true;
+const hasCompass = true;
+const hasSleepingBag = true;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
-  console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
-  console.log("You find your way to the village.");
-} else {
-  console.log("You get lost and wander aimlessly.");
+if (choice === "mountains")
+{
+  const torchChoice = readline.question("Do you have torch?");
+    if (torchChoice === "yes" && hasTorch){
+      console.log("You safely navigate through the dark mountains.");
+    }
+    else if (torchChoice == "no" || (!hasTorch)) {
+      console.log("It's too dark to proceed. Better stay and start tomorrow");
+      const tentChoice = readline.question("Do you have lightweight tent?");
+      if (tentChoice == "yes" && hasTent){
+        console.log("Set up the tent and then spend the night");
+      }
+      else if (tentChoice == "no" || !hasTent){
+        const sleepingBagChoice = readline.question("Do you have sleeping Bag?");
+        if (sleepingBagChoice == "yes" && hasSleepingBag){
+          console.log("Sleep and start tomorrow");
+        }
+        else if (sleepingBagChoice == "no" || !hasSleepingBag) {
+          console.log("Go to safe place and stay calm");
+
+        }
+      }
+} 
 }
 
-/* 
-
-Add Customization and expand the game:
-  - Add more choices and scenarios.
-  - Include additional items (e.g., a sword, a compass).
-  - Use nested conditionals and logical operators to create complex outcomes.
-
-*/
+else if (choice === "village") 
+  {
+  const mapChoice = readline.question("Do you have Map?");
+  if (mapChoice === "yes"  && (!hasMap))
+  {
+    console.log("You find your way to the village.");
+  }
+  else if (mapChoice === "no") {
+    const compassChoice = readline.question("Do you have compass?");
+    if (compassChoice === "yes" && hasCompass)
+    {
+    console.log("You find the direction using compass to proeceed the further path.");
+    }
+    else if (compassChoice === "no" || (!hasCompass))
+    {
+    console.log("Dont proceed, Wait for help");
+  }
+}
+}
+ else {
+  console.log("You get lost and wander aimlessly.");
+}
